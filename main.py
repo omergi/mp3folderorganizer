83f2a6a809d477431f2003394758c66e52559f6f
omergi <omergilat1@gmail.com>
2015-08-22 18:22:38 +0300

    Add main.py file

---
+++ main.py
@@ -0,0 +1,45 @@
+#!/usr/bin/pyhton
+# ---------------------- imports ------------------------------------------------
+import eyed3
+import sys
+import glob
+import os
+import shutil
+
+# ---------------------- implementaion ------------------------------------------
+
+def get_artist(audiofile):
+    return audiofile.tag.artist
+
+def create_dir(dir_name):
+    full_dir_name = os.path.join('Music_Sort','%s' % (dir_name, ))
+    if not os.path.exists(full_dir_name):
+        os.makedirs(full_dir_name)
+    return full_dir_name
+
+
+def main():
+    if len(sys.argv) < 1:
+        print 'No path given'
+    path = sys.argv[1]
+    files_list = glob.glob(os.path.join(path, '*.mp3'))
+    audiofiles_lst = [eyed3.load(fname) for fname in files_list]
+    for i in audiofiles_lst:
+        artist = get_artist(i)
+        dir_name = create_dir(artist)
+        try:
+            dest_path = os.path.join(dir_name, '%s - %s.mp3' % (artist, i.tag.title.strip()))
+        except Exception:
+            with open('log.txt', 'ab') as f:
+                f.write(i.path)
+        if not os.path.exists(dest_path):
+            try:
+                shutil.copy(i.path, dest_path)
+            except Exception:
+                with open('log.txt', 'ab') as f:
+                    f.write(i.path)
+
+
+
+if __name__ == "__main__":
+    main()
